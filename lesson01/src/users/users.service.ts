89d5d0e859f14bd54e24ec35fabf1e9071965f2f
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-02-03 18:40:03 +0100

    Update on service.tx

--- lesson01/src/users/users.service.ts
+++ lesson01/src/users/users.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      "id" : 1,
+      "name" : "Leanne Graham",
+      "email" : "sincere@april.biz" ,
+      "role" : "INTERN"
+     },
+    {
+      "id" : 1,
+      "name" : "Leanne john",
+      "email" : "apology@april.biz" ,
+      "role" : "ENGINEER"
+     },
+    {
+      "id" : 1,
+      "name" : "jingle",
+      "email" : "sinc@april.biz" ,
+      "role" : "ADMIN"
+     },
+    {
+      "id" : 1,
+      "name" : "Leanne TUNDE Graham",
+      "email" : "sincere@april.biz" ,
+      "role" : "ENGINEER"
+     },
+    {
+      "id" : 1,
+      "name" : "Leanne Graham",
+      "email" : "sincere@april.biz" ,
+      "role" : "INTERN"
+     }
+  ]
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN' ){
+    if(role){
+      return this.users.filter(user => user.role === role )
+    }
+
+    return this.users
+  }
+
+  findOne(id: number){
+    const user = this.users.find(user => user.id === id)
+
+    return user
+  }
+}
